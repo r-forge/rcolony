4bc5c677563fbfa9c2ce8771cbe00c3ca23c2a24
jonesor (Dr ) <jonesor@gmail.com>
2009-04-20 16:33:44 +0000

    Totally rewrote the monitor.colony.R function so that it grabs the stdout files. Works for Unix/OS X ONLY at the moment.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rcolony@11 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/monitor.colony.R
+++ pkg/R/monitor.colony.R
@@ -1,44 +1,59 @@
-#Dynamic plotting experiments
+
 monitor.colony<-function(datadir,interv.t=3,last.few=10){
-  if(length(list.files(datadir,pattern=".Ne"))>0){stop("There are already files present in output directory. Remove them and start again.")}
+
+ # if(length(list.files(datadir,pattern=".Ne"))>0){stop("There are already files present in output directory. Remove them and start again.")}
+
+
+MidResult<-NULL
+
   #Get start time
   t1<-Sys.time()
   
   elapsed.t<-0
-  while(length(list.files(datadir,pattern=".Ne"))<1){#run forever
+  
+  #while(length(list.files(datadir,pattern=".Ne"))<1){#run forever
+  while(TRUE){#run forever
+  
   t2<-Sys.time()
   elapsed.t<-as.numeric(t2-t1)
   
   if(elapsed.t>interv.t){
   
-  fileok<-length(list.files(datadir,pattern=".MidResult"))>0
-  col.names<-c("Date","Time","Run","Tmr","NumIterate","CrLogL","F1","F2","F3","FS","HSPair","FSPair","AssgnC1","AssgnC2","AssgnP1","AssgnP2")
+  fileok<-file.exists("temp.txt")
+  
+if(fileok==TRUE&!is.na(get.interm.data(datadir=datadir)[1,1])){
+	MidResult2<-get.interm.data(datadir=datadir)
+	MidResult<-rbind(MidResult,MidResult2)
+	MidResult<-unique(MidResult)
+
+x<-MidResult[,1]
+y<-MidResult[,2]
+  
+plot(x,y,type="b",xlab="",ylab="")
+title(xlab="NumIterate",ylab="CrLogL")
+
+#add routine to delete contents if file is too big.
+#50lines?
+
+tmp<-readLines(paste(datadir,"temp.txt",sep=""))
+n<-50
+from<-if(length(tmp)-n>0){length(tmp)-n>0}else{1}
+to<-length(tmp)
+
+write.table(tmp[from:to],file="temp.txt",row.names=FALSE,col.names=FALSE,quote=FALSE)
+print(MidResult)
+cat("\n")
+
+	}else{cat("Waiting...\n")}
   
   
-  if(fileok==TRUE){
-  
-  paste(datadir,list.files(pattern=".MidResult")[1],sep="")
-  MidResult<-read.table(paste(datadir,list.files(datadir,pattern=".MidResult")[1],sep=""),header=FALSE,col.names=col.names,skip=1)
-  
-  if(length(MidResult$NumIterate)<1){MidResult<-as.data.frame(matrix(rep(0,length(col.names)),ncol=length(col.names)))}
-  
-  
-  }else{
-  
-  MidResult<-as.data.frame(matrix(rep(0,length(col.names)),ncol=length(col.names)))}
-  
-  names(MidResult)<-col.names
-  x<-MidResult$NumIterate
-  y<-MidResult$CrLogL
-  
-  plot(x,y,type="b",xlab="",ylab="")
-  title(xlab="NumIterate",ylab="CrLogL")
-  #legend("bottomright","Still Running...",text.col="red")
-  
+ 
   t1<-Sys.time()
-  }
   
   
   
   }
-  }
+  }}
+
+
+
